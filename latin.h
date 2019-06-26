7b60cc86776313c7db1e1f62f559bdb325581935
Anlast <48757061+Anlast@users.noreply.github.com>
2019-06-26 20:55:35 +0100

    Update latin.h for semi-latin squares

--- latin.h
+++ latin.h
@@ -13,12 +13,20 @@ extern int solver_show_working, solver_recurse_depth;
 
 struct latin_solver {
   int o;                /* order of latin square */
+#ifdef SEMI_LATIN
+  int depth;			/* depth of latin squar */
+#endif
   unsigned char *cube;  /* o^3, indexed by x, y, and digit:
                            true in that position indicates a possibility */
   digit *grid;          /* o^2, indexed by x and y: for final deductions */
 
   unsigned char *row;   /* o^2: row[y*cr+n-1] true if n is in row y */
   unsigned char *col;   /* o^2: col[x*cr+n-1] true if n is in col x */
+
+#ifdef SEMI_LATIN
+  bool *force;			/* o^2: force[y*cr+x] true if cell must contain a value */
+  bool *forbid;			/* o^2: forbid[y*cr+x] true if cell must be blank */
+#endif
 
 #ifdef STANDALONE_SOLVER
   char **names;         /* o: names[n-1] gives name of 'digit' n */
@@ -32,6 +40,11 @@ struct latin_solver {
 
 
 /* --- Solver individual strategies --- */
+
+#ifdef SEMI_LATIN
+/* Figure out which cells must have or _not_ have a value */
+int latin_solver_distribute_force_forbid(struct latin_solver *solver);
+#endif
 
 /* Place a value at a specific location. */
 void latin_solver_place(struct latin_solver *solver, int x, int y, int n);
@@ -64,7 +77,11 @@ int latin_solver_forcing(struct latin_solver *solver,
  * Will allocate members of snew, but not snew itself
  * (allowing 'struct latin_solver' to be the first element in a larger
  * struct, for example). */
-void latin_solver_alloc(struct latin_solver *solver, digit *grid, int o);
+void latin_solver_alloc(struct latin_solver *solver, digit *grid, int o
+#ifdef SEMI_LATIN
+						, int depth
+#endif
+						);
 void latin_solver_free(struct latin_solver *solver);
 
 /* Allocates scratch space (for _set and _forcing) */
